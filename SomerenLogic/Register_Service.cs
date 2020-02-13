fe820a666abbed86368436a19ee25a2973a8de61
Kingwoods <42969169+MohaymenSameen@users.noreply.github.com>
2020-02-13 20:18:15 +0100

    Add files via upload

---
+++ SomerenLogic/Register_Service.cs
@@ -0,0 +1,29 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenLogic
+{
+    public class Register_Service
+    {
+        Register_DAO register_db = new Register_DAO();
+
+        public List<Register> GetRegisters()
+        {
+            try
+            {
+                List<Register> register = register_db.Db_Get_All_Register();
+                return register;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Someren couldn't connect to the database");
+            }
+        }
+    }
+}
